59fa131435b19bc30889d75a3ed473d14ff09d84
falexh <38890394+falexh@users.noreply.github.com>
2018-06-28 21:33:03 -0700

    Update cat.js

--- hackproject1/cat.js
+++ hackproject1/cat.js
@@ -6,16 +6,13 @@ function Cat(x,y,m,img)  // for the array of cat pictures
   this.pos = createVector(x,y);
   this.vel = createVector(0,0);
   this.acc = createVector(0,0);
-  // this.img = random(cats);
   this.mass = m;
-
   this.cat = random(cats);
 
 //show the images of the cats
   this.show = function()
   {
     imageMode(CENTER);
-    // image(this.cat, x, y, 80, this.m);
     image(this.cat,this.pos.x, this.pos.y, this.mass*30, this.mass*30,);
   }
 
